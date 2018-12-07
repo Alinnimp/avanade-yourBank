90a4d4bb1dc6e6d489f484c49dedf8a5f116fd0e
Juliana Reis <ju07reis@hotmail.com>
2018-12-07 15:33:08 -0200

    ajustes no formulario de transferencia

---
+++ src/app/components/transferencia-confirm/transferencia-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transferencia-confirm',
+  templateUrl: './transferencia-confirm.component.html',
+  styleUrls: ['./transferencia-confirm.component.css']
+})
+export class TransferenciaConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
